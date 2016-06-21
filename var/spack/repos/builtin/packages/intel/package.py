433672d0029999aa115591afacda229eda73d273
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2016-06-21 14:44:50 +0200

    added mock package for intel

---
+++ var/spack/repos/builtin/packages/intel/package.py
@@ -0,0 +1,15 @@
+from spack import *
+
+class Intel(Package):
+    """
+    Mock package for Intel compilers
+    """
+
+    homepage = "http://www.example.com"
+    url      = "http://www.example.com/intel-1.0.tar.gz"
+
+    version('16.0')
+
+    def install(self, spec, prefix):
+        pass
+
