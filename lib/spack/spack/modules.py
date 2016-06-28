a4394a33d5773292c0099e8b1cea80d3d2016826
alalazo <massimiliano.culpo@googlemail.com>
2016-06-28 17:59:34 +0200

    module : can regenerate single module files, homogenized cli options
    spack module :
    - refresh accepts a constraint
    - find and refresh share common cli options
    - ask for confirmation before refreshing
    - deleting the module file tree is now optional
    
    Conflicts:
    	lib/spack/spack/modules.py

--- lib/spack/spack/modules.py
+++ lib/spack/spack/modules.py
@@ -461,8 +461,7 @@ class EnvModule(object):
 
 class Dotkit(EnvModule):
     name = 'dotkit'
-    path = join_path(spack.share_path, "dotkit")
-
+    path = join_path(spack.share_path, 'dotkit')
     environment_modifications_formats = {
         PrependPath: 'dk_alter {name} {value}\n',
         SetEnv: 'dk_setenv {name} {value}\n'
@@ -474,7 +473,7 @@ class Dotkit(EnvModule):
 
     @property
     def file_name(self):
-        return join_path(Dotkit.path, self.spec.architecture,
+        return join_path(self.path, self.spec.architecture,
                          '%s.dk' % self.use_name)
 
     @property
@@ -503,7 +502,6 @@ class Dotkit(EnvModule):
 class TclModule(EnvModule):
     name = 'tcl'
     path = join_path(spack.share_path, "modules")
-
     environment_modifications_formats = {
         PrependPath: 'prepend-path --delim "{delim}" {name} \"{value}\"\n',
         AppendPath: 'append-path   --delim "{delim}" {name} \"{value}\"\n',
@@ -523,7 +521,7 @@ class TclModule(EnvModule):
 
     @property
     def file_name(self):
-        return join_path(TclModule.path, self.spec.architecture, self.use_name)
+        return join_path(self.path, self.spec.architecture, self.use_name)
 
     @property
     def header(self):
