6828319de3688b91f8477bb20b293dfa1ee3781e
Massimiliano Culpo <massimiliano.culpo@googlemail.com>
2016-07-06 15:53:43 +0200

    suite-sparse : compiles with intel

--- var/spack/repos/builtin/packages/mkl/package.py
+++ var/spack/repos/builtin/packages/mkl/package.py
@@ -21,4 +21,4 @@ class Mkl(Package):
         """
         For dependencies, make mpicc's use spack wrapper.
         """
-        pass
+        spack_env.set('MKLROOT', '/ssoft/spack/external/intel/2016/compilers_and_libraries_2016.3.210/linux/mkl')
