fd10952eaab10cebb3be124dc63aa639f3141ef7
Nicolas Richart <nicolas.richart@epfl.ch>
2016-07-08 15:56:37 +0200

    adding scalpack interface to mkl

--- var/spack/repos/builtin/packages/mkl/package.py
+++ var/spack/repos/builtin/packages/mkl/package.py
@@ -22,3 +22,22 @@ class Mkl(Package):
         For dependencies, make mpicc's use spack wrapper.
         """
         spack_env.set('MKLROOT', '/ssoft/spack/external/intel/2016/compilers_and_libraries_2016.3.210/linux/mkl')
+        pass
+
+    def setup_dependent_package(self, module, dependent_spec):
+        spec = self.spec
+        
+        if 'intelmpi' in spec or 'mvapich2' in spec:
+            libblacs='-lmkl_blacs_intelmpi_lp64'
+        elif 'openmpi' in spec:
+            libblacs='-lmkl_blacs_openmpi_lp64'
+        else:
+            libblacs='-lmkl_blacs_lp64'
+
+        mkl_lib = join_path(spec.prefix.lib, 'lib', 'intel64')
+        spec.fc_link = '-L{0} {1} {2} {3}'.format(mkl_lib,
+                                                  '-lmkl_scalapack_lp64', libblacs,
+                                                  '-lmkl_intel_lp64 -lmkl_core -lmkl_sequential')
+        spec.cc_link = '-L{0} {1} {2} {3}'.format(mkl_lib,
+                                                  '-lmkl_scalapack_lp64', libblacs,
+                                                  '-lmkl_intel_lp64 -lmkl_core -lmkl_sequential')
