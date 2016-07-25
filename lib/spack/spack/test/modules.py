7b9703ef7b3ae0d92fc5187672bb32716a3c7592
alalazo <massimiliano.culpo@googlemail.com>
2016-07-25 18:22:09 +0200

    qa : flake8 issues fixes #1109

--- lib/spack/spack/test/modules.py
+++ lib/spack/spack/test/modules.py
@@ -275,7 +275,8 @@ class TclTests(ModuleFileGeneratorTests):
         self.assertEqual(
             len([x for x in content if 'setenv FOO "foo"' in x]), 1)
         self.assertEqual(len([x for x in content if 'unsetenv BAR' in x]), 1)
-        self.assertEqual(len([x for x in content if 'setenv MPILEAKS_ROOT' in x]), 1)
+        self.assertEqual(
+            len([x for x in content if 'setenv MPILEAKS_ROOT' in x]), 1)
 
         spec = spack.spec.Spec(libdwarf_spec_string)
         content = self.get_modulefile_content(spec)
@@ -290,7 +291,8 @@ class TclTests(ModuleFileGeneratorTests):
             len([x for x in content if 'is-loaded foo/bar' in x]), 1)
         self.assertEqual(
             len([x for x in content if 'module load foo/bar' in x]), 1)
-        self.assertEqual(len([x for x in content if 'setenv LIBDWARF_ROOT' in x]), 1)
+        self.assertEqual(
+            len([x for x in content if 'setenv LIBDWARF_ROOT' in x]), 1)
 
     def test_blacklist(self):
         spack.modules.CONFIGURATION = self.configuration_blacklist
