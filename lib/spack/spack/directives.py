2616d002adb5fac92cc9ae61f98142e84513df78
alalazo <massimiliano.culpo@googlemail.com>
2016-11-23 21:34:06 +0100

    variant directive: added group validator for edge cases with complex logic (see mvapich2)

--- lib/spack/spack/directives.py
+++ lib/spack/spack/directives.py
@@ -366,7 +366,7 @@ def patch(url_or_filename, level=1, when=None, **kwargs):
 
 
 @directive('variants')
-def variant(name, default=False, description='', values=(True, False), exclusive=True):  # NOQA: ignore=E501
+def variant(pkg, name, default=False, description='', values=(True, False), exclusive=True, validator=None):  # NOQA: ignore=E501
     """Define a variant for the package. Packager can specify a default
     value (on or off) as well as a text description."""
 
@@ -379,7 +379,9 @@ def variant(name, default=False, description='', values=(True, False), exclusive
             msg = "Invalid variant name in {0}: '{1}'"
             raise DirectiveError(directive, msg.format(pkg.name, name))
 
-            pkg.variants[name] = Variant(name, default, description, values, exclusive)
+        pkg.variants[name] = Variant(
+            name, default, description, values, exclusive, validator
+        )
     return _execute
 
 
