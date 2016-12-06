13efacfc29b0bcbd88b2da73f1196473d3a0ec05
alalazo <massimiliano.culpo@googlemail.com>
2016-12-06 12:23:17 +0100

    qa: optimized imports

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -22,10 +22,10 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+
 import os
 
 from spack import *
-import os
 
 
 def _verbs_dir():
