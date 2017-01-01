b1f6faa131237f8b0310fb65d4941b7e90d354b4
alalazo <massimiliano.culpo@googlemail.com>
2017-01-01 12:09:50 +0100

    variant: the default value for the 'default' argument is None. This will be translated to False for a bool variant and to an empty string for a multi-valued variant.

--- var/spack/repos/builtin/packages/mvapich2/package.py
+++ var/spack/repos/builtin/packages/mvapich2/package.py
@@ -60,7 +60,6 @@ class Mvapich2(AutotoolsPackage):
 
     variant(
         'process_managers',
-        default='',
         description='List of the process managers to activate',
         values=('slurm', 'hydra', 'gforker', 'remshell'),
         exclusive=False,
@@ -69,7 +68,6 @@ class Mvapich2(AutotoolsPackage):
 
     variant(
         'fabrics',
-        default='',
         description='The fabric enabled for this build',
         values=(
             'psm', 'sock', 'nemesisib', 'nemesis', 'mrail', 'nemesisibtcp'
