64ad96bdb75efa9a8353d5951326d7e090302f1a
alalazo <massimiliano.culpo@googlemail.com>
2017-02-25 14:05:48 +0100

    module refresh: changed behavior when no module type is specified
    Modifications:
      - spack module refresh without `--module-type` specified tries to regenerate all known module types
      - spack module rm without `--module-type` specified tries to delete all known module types
      - added tests for the new behavior

--- lib/spack/spack/cmd/common/arguments.py
+++ lib/spack/spack/cmd/common/arguments.py
@@ -81,7 +81,7 @@ _arguments['constraint'] = Args(
 _arguments['module_type'] = Args(
     '-m', '--module-type',
     choices=spack.modules.module_types.keys(),
-    default=list(spack.modules.module_types.keys())[0],
+    action='append',
     help='type of module files [default: %(default)s]')
 
 _arguments['yes_to_all'] = Args(
